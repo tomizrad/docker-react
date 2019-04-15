0c28015f9cd54279073ee0b1f8e03621a0923caa
thomasw <tomizrad@gmail.com>
2019-04-14 22:32:15 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi, I'm tom =)
+            Hey look, its madison 8====D
           </p>
           <a
             className="App-link"
